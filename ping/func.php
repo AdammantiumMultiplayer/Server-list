be5ab2e9102ea2ebd07263a123b11416f9f0c6a7
Adammantium <adam@devforce.de>
2023-11-05 16:15:47 +0100

    New Server IPs

--- ping/func.php
+++ ping/func.php
@@ -6,6 +6,10 @@ function getAddress() {
 		$address = "de-amp.adamite.de";
 	} else if($address == "127.0.0.1") {
 		$address = "de-amp.adamite.de";
+	} else if($address == "193.203.238.183") {
+		$address = "de-amp.adamite.de";
+	} else if($address == "191.101.206.36") {
+		$address = "us-amp.adamite.de";
 	} else if($address == "85.215.226.233") {
 		$address = "nexusrealms.de";
 	} else {
